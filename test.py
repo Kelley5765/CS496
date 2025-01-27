17e112762b7a3667fa40be790054c938974b4981
Kelley5765 <mkelley3459@sdsu.edu>
2025-01-26 19:18:56 -0800

    Test commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print('Mission Control is Ready!')
 
 print('git remote set-url origin https://github.com/Kelley5765/CS496.git')
-print('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')
+
 
 age = input('What is your age?')
 height = int(input('What is your height?'))
